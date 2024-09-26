bc9f43e53a2555468a9d731c20d71002824c1730
LuwamG <30065081@cc.peralta.edu>
2024-09-25 21:33:13 -0700

    Update Binary.cpp

--- Binary/Binary.cpp
+++ Binary/Binary.cpp
@@ -1,35 +1,34 @@
-// Binary.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include<string>
-#include <cmath>
 using namespace std;
 
-	int number;
-	long binary=0;
-	int reminder;
-	int	i=1;
-	long convertToBinary(int number) {
+long convertToBinary(int number) { // Function to convert decimal to binary representation
+    if (number < 0) {
+        cout << "Error! The number is negative." << endl;
+        return -1;
+    }
 
-		while (number >= 0) {
-			reminder = number % 2;
-			number = number / 2;
-			binary = binary + reminder * i;
-			i = i * 10;
-		}
-		return binary;
-	}
-	int main() {
-		int number;
-		int binary;
-		cout << "Enter a postive decimal number: ";
-		cin >> number;
-		if (number < 0) {
-			cout << "Error! the number is negative." << endl;
-		}
-		binary = convertToBinary(number);
-		cout << number << " in binary representation is " << binary << endl;
-		return 0;
-	}
+    long binary = 0;
+    int reminder;
+    int i = 1;
 
+    while (number != 0) {
+        reminder = number % 2;
+        number = number / 2;
+        binary = binary + reminder * i;
+        i = i * 10;
+    }
+    return binary;
+}
+     
+int main() {
+    int number;
+    cout << "Enter a positive decimal number: ";
+    cin >> number;
+
+    long binary = convertToBinary(number); // Call the function
+    if (binary != -1) { 
+        cout << number << " in binary representation is " << binary << endl;
+    }
+
+    return 0;
+}
